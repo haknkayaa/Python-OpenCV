ab40c14fb48aab13f6ff4a04c28ad6c0cb956ab8
Hakan KAYA <hakan_kaya67@hotmail.com>
2016-09-05 05:06:54 +0300

    update

---
+++ videoProcessing/4-DrawingLine.py
@@ -0,0 +1,24 @@
+# @title      :  Drawing Line
+# @author     :  Hakan Kaya
+# @date       :  03.09.2016
+# @description:  Cizgi cizimleri
+
+import numpy as np
+import cv2
+
+
+# Siyah backgroung yaratma
+img = np.zeros((512, 512, 3), np.uint8)
+
+# Cizgi yaratma
+#              image, (ilk nokta), (son nokta), renk, kalinlik
+img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
+
+# Ekrana basma
+cv2.imshow("img",img)
+
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    print "Cikis yapildi"
+    cv2.destroyAllWindows()
+
+
