52c95a98b243ca53cb25bb0dda5198ad3d4c3b04
Hakan <haknkayaa@gmail.com>
2016-09-21 16:44:33 +0300

    update

--- videoProcessing/8-resimlerdeDonusumler.py
+++ videoProcessing/8-resimlerdeDonusumler.py
@@ -1,4 +1,4 @@
-# @title      :  Goruntu Aygitindan Goruntu Alma
+# @title      :  resim transformasyonu
 # @author     :  Hakan Kaya
 # @date       :  03.09.2016
 # @update     :  11.09.2016 by Hakan Kaya
